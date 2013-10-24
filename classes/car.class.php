3ed778add7148c1043f1939dc1d40a83b9562f7a
kph1g10 <kph1g10@ecs.soton.ac.uk>
2013-10-24 15:01:35 +0100

    created php class for car attributes
    A php class created to contain the attributes of the cars in
    carData.php.

---
+++ classes/car.class.php
@@ -0,0 +1,22 @@
+<?php
+	class carAttributes{
+		private $make;
+		private $model;
+		private $colour;
+		private $description;
+		private $price;
+		private $reg_number;
+		private $reg_year;
+		
+		function_construct($make, $model, $colour, $description, $price, $reg_number, $reg_year){
+		
+			$this->make = $make;
+			$this->model = $model;
+			$this->colour = $colour;
+			$this->description = $description;
+			$this->price = $price;
+			$this->reg_number = $reg_number;
+			$this->reg_year = $reg_year;
+		}
+	}
+?>
